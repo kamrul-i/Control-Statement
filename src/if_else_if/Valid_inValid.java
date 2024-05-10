6547e6b6d8496eed564572c3cf3ccf8132119788
kamrul-i <komrul45@gmail.com>
2024-05-10 11:05:34 +0200

    used try-catch.

--- src/if_else_if/Valid_inValid.java
+++ src/if_else_if/Valid_inValid.java
@@ -5,36 +5,37 @@ import java.util.Scanner;
 public class Valid_inValid {
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-		
-		//=====================================================
-		int number;
-		
-		System.out.print("Enter any number : ");
-		number = input.nextInt();
-		
-		if(number == 1){
-		    System.out.print("One");
-		}else if(number == 2){
-		    System.out.print("Two");
-		}else if(number == 3){
-		    System.out.print("Three");
-		}else if(number == 4){
-		    System.out.print("Four");
-		}else if(number == 5){
-		    System.out.print("Five");
-		}else if(number == 6){
-		    System.out.print("Six");
-		}else if(number == 7){
-		    System.out.print("Seven");
-		}else if(number == 8){
-		    System.out.print("Eight");
-		}else if(number == 9){
-		    System.out.print("Nine");
-		}else{
-		    System.out.print("Invalid Number");
+		try (Scanner input = new Scanner(System.in)) {
+			
+			//=====================================================
+			int number;
+			
+			System.out.print("Enter any number : ");
+			number = input.nextInt();
+			
+			if(number == 1){
+			    System.out.print("One");
+			}else if(number == 2){
+			    System.out.print("Two");
+			}else if(number == 3){
+			    System.out.print("Three");
+			}else if(number == 4){
+			    System.out.print("Four");
+			}else if(number == 5){
+			    System.out.print("Five");
+			}else if(number == 6){
+			    System.out.print("Six");
+			}else if(number == 7){
+			    System.out.print("Seven");
+			}else if(number == 8){
+			    System.out.print("Eight");
+			}else if(number == 9){
+			    System.out.print("Nine");
+			}else{
+			    System.out.print("Invalid Number");
+			}
+			//=====================================================
 		}
-		//=====================================================
 	}
 
 }
