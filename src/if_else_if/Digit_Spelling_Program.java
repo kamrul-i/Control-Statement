d8f0e43bc2fecae231a162c6ee9732aefad9d4be
kamrul-i <komrul45@gmail.com>
2024-05-10 13:16:51 +0200

    if-else statement use "== equal"

---
+++ src/if_else_if/Digit_Spelling_Program.java
@@ -0,0 +1,39 @@
+package if_else_if;
+
+import java.util.Scanner;
+
+public class Digit_Spelling_Program {
+	
+	public static void main(String[] arg) {
+
+		try (Scanner input = new Scanner(System.in)) {
+			int number;
+			
+			System.out.print("Enter any number : ");
+			number = input.nextInt();
+			
+			if(number == 1){
+			    System.out.print("One");
+			}else if(number == 2){
+			    System.out.print("Two");
+			}else if(number == 3){
+			    System.out.print("Three");
+			}else if(number == 4){
+			    System.out.print("Four");
+			}else if(number == 5){
+			    System.out.print("Five");
+			}else if(number == 6){
+			    System.out.print("Six");
+			}else if(number == 7){
+			    System.out.print("Seven");
+			}else if(number == 8){
+			    System.out.print("Eight");
+			}else if(number == 9){
+			    System.out.print("Nine");
+			}else{
+			    System.out.print("Invalid Number");
+			}
+		}
+	}
+
+}
