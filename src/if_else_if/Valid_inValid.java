bb1c72ea990f05fd5d84d64cbd971fb3fb06c13a
kamrul-i <komrul45@gmail.com>
2024-05-10 11:29:23 +0200

    voter check.

--- src/if_else_if/Valid_inValid.java
+++ src/if_else_if/Valid_inValid.java
@@ -8,56 +8,18 @@ public class Valid_inValid {
 		try (Scanner input = new Scanner(System.in)) {
 			
 			//=====================================================
-			int number;
-			
-			System.out.print("Enter any number : ");
-			number = input.nextInt();
-			
-			if(number == 1){
-			    System.out.print("One");
-			}else if(number == 2){
-			    System.out.print("Two");
-			}else if(number == 3){
-			    System.out.print("Three");
-			}else if(number == 4){
-			    System.out.print("Four");
-			}else if(number == 5){
-			    System.out.print("Five");
-			}else if(number == 6){
-			    System.out.print("Six");
-			}else if(number == 7){
-			    System.out.print("Seven");
-			}else if(number == 8){
-			    System.out.print("Eight");
-			}else if(number == 9){
-			    System.out.print("Nine");
-			}else{
-			    System.out.print("Invalid Number");
-			}
+	        int age;
+	        System.out.print("Enter any age : ");
+	        age = input.nextInt();
+	        
+	        if(age>17){
+	            System.out.print("Valid Voter");
+	        }else{
+	            System.out.print("inValid Voter");
+	            
+	        }
 			//=====================================================
-			
-			
-			
-			//একই design কে switch style -এ দেখানো হয়েছে।।
-			System.out.println("\n\n");
-			
-			int num2;
-			System.out.print("Enter any number : ");
-			num2 = input.nextInt();
-			
-			switch(num2) {
-				case 1 -> System.out.print("one");
-				case 2 -> System.out.print("two");
-				case 3 -> System.out.print("three");
-				case 4 -> System.out.print("four");
-				case 5 -> System.out.print("five");
-				case 6 -> System.out.print("sixx");
-				case 7 -> System.out.print("seven");
-				case 8 -> System.out.print("eight");
-				case 9 -> System.out.print("nine");
-				default -> System.out.print("Smilling !");
-			
-			}
+		
 			System.out.println("\n\n");
 		}
 	}
