73bc70f38eb4021beff8b2edfb90b69d7a56f9db
kamrul-i <komrul45@gmail.com>
2024-05-20 21:04:36 +0600

    "While-loop" vs "Do-while-Loop" problem solve.

--- src/loop_Demo/While_vs_Do_while_Loop.java
+++ src/loop_Demo/While_vs_Do_while_Loop.java
@@ -3,16 +3,31 @@ package loop_Demo;
 public class While_vs_Do_while_Loop {
 	public static void main(String[] args) {
 		
+		
+        //Compare with 'while loop' & 'do while loop'
+		//While-loop_vs_Do-while-Loop এর মধ্যে পার্থক্য print করে দেখানো হয়েছে।।
+        
+		//------------------------------------------
+        int p = 6;
+        while(p <= 5){
+            System.out.println("loop");
+            p++;
+        }
+        System.out.println();
+        //------------------------------------------
 
         int b = 7;
         do{
             System.out.println(" loop");
             b++;
-        }while(b <= 5);   
+        }while(b <= 5); 
+        //------------------------------------------
         
-        // condition এর মান =5 ধরা হয়েছে, এবং variable এর মান =7 ধরা হয়েছে।।
-        // variable এর মান =7 ধরা হলেও function টি ১ বার print হয়েছে।। 
-		// "Do-while-loop" function-এ condition থেকে variable এর মান বেশি হলেও, print-এ result ১ বার আসবে।।  
+
+        //'while loop' & 'do while loop' এর মধ্যে পার্থক্য হলো-
+		// "while loop" function-এ condition থেকে variable এর মান বেশি হলে, print-এ কোন result আসবে না।।
+		// "Do-while-loop" function-এ condition থেকে variable এর মান বেশি হলেও, print-এ result ১ বার আসবে।। 
+        
         
 	}
 
