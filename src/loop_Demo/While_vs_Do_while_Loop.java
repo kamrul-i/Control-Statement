0616eb322f9b7f2c178f16509173fb1c3a85e778
kamrul-i <komrul45@gmail.com>
2024-05-20 20:32:23 +0600

    while loop print successful !

---
+++ src/loop_Demo/While_vs_Do_while_Loop.java
@@ -0,0 +1,20 @@
+package loop_Demo;
+
+public class While_vs_Do_while_Loop {
+	public static void main(String[] args) {
+		
+		
+
+
+		//স্বাভাবিকভাবে এ function টি print হয়েছে দেখা যাচ্ছে।।
+		int i = 1;
+        
+        while(i <= 8){
+            System.out.println(i+" kamrul");
+            i++;
+        }
+        System.out.println();   
+        
+	}
+
+}
