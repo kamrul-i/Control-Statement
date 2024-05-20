4b8977652f2aafda4827094b30581808cea2e76d
kamrul-i <komrul45@gmail.com>
2024-05-20 22:03:07 +0600

    Sum of ordinal numbers.

--- src/loop_Demo/For_Loop.java
+++ src/loop_Demo/For_Loop.java
@@ -1,57 +1,28 @@
 package loop_Demo;
 
+import java.util.Scanner;
+
 public class For_Loop {
 	public static void main(String[] args) {
 		
 		
-		//একই word/sentence কে যতবার print করতে চান, এই loop টি ব্যবহার করুন।।
-		for (int  i=1;  i<=5;  i++){
-            System.out.println("Bangladesh");
-        }
 		
-		System.out.printf("\n");
-        
-		
-		
-		//কতবার print হয়েছে, তার পাশে serial number দেখতে চান, এই loop টি ব্যবহার করুন।।
-        for (int  i=1;  i<=7;  i++){
-            System.out.println(i+" Bangladesh");
-        }
-        System.out.println();
-        
-        
-        
-        //মনে করেন, আপনি (১-৫০) পর্যন্ত গণনা লিখবেন, এই loop টি ব্যবহার করুন।।
-        for (int   i=1;  i<=50;  i++){
-            System.out.println(i);    
-        }
-        System.out.println();
-        
-        
-        
-      //আবার মনে করেন, আপনি (২০-১) পর্যন্ত গণনা লিখবেন, এই loop টি ব্যবহার করুন।।
-        for (int i = 20; i>=1; i--){
-            System.out.println(i);
-        }
-        System.out.println();
-        
-        
-        
-        
-        //জোড় সংখ্যা গণনা করতে চাইলে, এই loop টি ব্যবহার করুন।।
-        for (int   i=2;  i<=20;  i=i+2){
-            System.out.println(i);
-        }
-        System.out.println();
-        
-        
-        
-        
-        //বিজোড় সংখ্যা গণনা করতে চাইলে, এই loop টি ব্যবহার করুন।।
-        for (int   i=1;  i<=20; i=i+2){
-            System.out.println(i);
-        }
-        System.out.println();
+		//নিজের সুবিধামতো ধারাবাহিক/ক্রমিক সংখ্যার যোগফল বের করতে এ function ব্যবহার করুন।।
+		try (Scanner input = new Scanner(System.in)) {
+			
+			int sum = 0;
+			int m, n;
+			System.out.print("Enter first number : ");
+			m = input.nextInt();
+			
+			System.out.print("Enter last number : ");
+			n = input.nextInt();
+			
+			for (int i = m; i <= n; i++) {
+			    sum = sum + i;
+			}
+			System.out.print("The sum is : "+sum);
+		}
 
 	}
 
