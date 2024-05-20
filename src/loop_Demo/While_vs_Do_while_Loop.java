816ca8e1411c723358fda603f43b248ba8ec88fb
kamrul-i <komrul45@gmail.com>
2024-05-20 20:41:54 +0600

    "while loop" print fail !

--- src/loop_Demo/While_vs_Do_while_Loop.java
+++ src/loop_Demo/While_vs_Do_while_Loop.java
@@ -3,17 +3,22 @@ package loop_Demo;
 public class While_vs_Do_while_Loop {
 	public static void main(String[] args) {
 		
-		
-
-
-		//স্বাভাবিকভাবে এ function টি print হয়েছে দেখা যাচ্ছে।।
-		int i = 1;
-        
-        while(i <= 8){
-            System.out.println(i+" kamrul");
-            i++;
+	
+        int p = 6;
+        while(p <= 5){
+            System.out.println("loop");
+            p++;
         }
-        System.out.println();   
+        System.out.println();
+        
+
+        // condition এর মান =5 ধরা হয়েছে, এবং variable এর মান =6 ধরা হয়েছে।।
+        // variable এর মান =6 ধরার কারণে এ function টি print হয় নাই।। 
+		// "while loop" function-এ condition থেকে variable এর মান বেশি হলে, print-এ কোন result আসবে না।।
+        
+        
+        
+
         
 	}
 
